307292573861b255ee2ac5504e0b930f99962893
viswa3760 <viswa.bharath@dojima.network>
2024-04-12 16:32:50 +0530

    images change locally

--- configs/app/ui.ts
+++ configs/app/ui.ts
@@ -26,8 +26,8 @@ const HOMEPAGE_PLATE_BACKGROUND_DEFAULT = 'radial-gradient(103.03% 103.03% at 0%
 const UI = Object.freeze({
   sidebar: {
     logo: {
-      'default': getExternalAssetFilePath('NEXT_PUBLIC_NETWORK_LOGO'),
-      dark: getExternalAssetFilePath('NEXT_PUBLIC_NETWORK_LOGO_DARK'),
+      'default': 'icons/dark.svg',
+      dark: 'icons/light.svg',
     },
     icon: {
       // 'default': getExternalAssetFilePath('NEXT_PUBLIC_NETWORK_ICON'),
